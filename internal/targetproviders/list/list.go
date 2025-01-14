8b4e22a9018a130dc9b7b45ed94f3ac560807360
Paulo Almeida <almeidapaulopt@gmail.com>
2025-01-14 18:39:06 +0000

    fix: List proxies don't start

--- internal/targetproviders/list/list.go
+++ internal/targetproviders/list/list.go
@@ -138,13 +138,15 @@ func (c *Client) WatchEvents(_ context.Context, eventsChan chan targetproviders.
 	c.file.OnChange(c.onFileChange)
 
 	// start initial proxies
-	for k := range c.configProxies {
-		eventsChan <- targetproviders.TargetEvent{
-			ID:             k,
-			TargetProvider: c,
-			Action:         targetproviders.ActionStart,
+	go func() {
+		for k := range c.configProxies {
+			eventsChan <- targetproviders.TargetEvent{
+				ID:             k,
+				TargetProvider: c,
+				Action:         targetproviders.ActionStart,
+			}
 		}
-	}
+	}()
 }
 
 func (c *Client) onFileChange(e fsnotify.Event) {
